57fd979542869616291bb8efad1d6478ae0eb013
Steph Sapp <stephs1531@gmail.com>
2019-07-24 09:15:50 -0500

    add puppy.json array

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header () {
+    return (
+        <div className="row jumbotron valign-wrapper">
+            <h1>React Memory Game</h1>
+        </div>
+    )
+}
+
+export default Header
